3d4d9d4c3028132d23becf5bf106e21dbc3bcc52
Alan Calderari <alan.calderari@obraprimaweb.com>
2023-07-18 21:45:08 -0300

    create routes

--- MyApi/Program.cs
+++ MyApi/Program.cs
@@ -2,5 +2,14 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/name/{nome}", (string nome) => $"Hello {nome}!");
+
+app.MapPost("/", (User user) => user);
 
 app.Run();
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+}
